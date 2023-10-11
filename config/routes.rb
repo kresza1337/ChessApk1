2d52aee19a47e3085a0f5a11ec52daf78018ba50
kresza1337 <krechaxdkrechaxd@gmail.com>
2023-10-11 19:29:08 +0200

    Create App

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root 'pages#home'
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
